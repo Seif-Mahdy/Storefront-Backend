d7ecd331218c8f7f46fac056dd7582b2c7f04fc6
Seif Alaa <seif@cloudypedia.com>
2022-10-12 19:29:52 +0200

    attribute code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,24 @@
+/**
+ * url: https://github.com/prettier/eslint-config-prettier/
+ * date: 12-10-2022
+ */
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        'prettier'
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint",
-        'prettier'
-    ],
-    "rules": {
-        'prettier/prettier': 1,
-    }
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 1,
+  },
 }
